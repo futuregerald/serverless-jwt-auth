41fcd66757a99b32cabd1fd0f96588d00504ef12
Gerald Onyango <gerald.onyango@gmail.com>
2021-01-05 23:51:36 -0500

    adding typescript

---
+++ src/http/post-auth-signup/index.ts
@@ -0,0 +1,17 @@
+require('../DB/dbConnection');
+
+const { signupEmailPassword } = require('../auth/auth');
+
+exports.handler = async function http(req) {
+  const body = JSON.parse(req.body);
+  const results = await signupEmailPassword(body);
+  return {
+    statusCode: 200,
+    headers: {
+      'cache-control':
+        'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
+      'content-type': 'text/html; charset=utf8',
+    },
+    body: JSON.stringify(results),
+  };
+};
