1ec8a0cff98eca673a21dfd216016663cf60098c
Gerald Onyango <gerald.onyango@gmail.com>
2021-01-08 21:04:44 -0500

    fixing

--- src/http/DB/UserModel.ts
+++ src/http/DB/UserModel.ts
@@ -3,7 +3,6 @@ import mongoose, { Schema, Document } from 'mongoose';
 const argon2 = require('argon2');
 
 export interface IUser extends Document {
-  _id?: string;
   email?: string;
   password?: string;
   appMetadata?: any;
