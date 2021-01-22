dc3eb69491506b0abe65b76ea3f79525cc278b48
Gerald Onyango <gerald.onyango@gmail.com>
2021-01-21 23:53:17 -0500

    added verification tokens, cleaned up some functions, added signup event

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -29,12 +29,6 @@ export interface signupEmailPasswordFunc {
   password: string;
 }
 
-export interface signupReturn {
-  user: any;
-  jwt: string;
-  refreshToken: string;
-}
-
 export interface IUser extends Document {
   _id?: any;
   email?: string;
@@ -48,12 +42,25 @@ export interface IUser extends Document {
   isValidPassword?: (string) => Promise<boolean>;
 }
 
+export interface signupReturn {
+  user: IUser;
+  jwt: string;
+  refreshToken: string;
+}
+
 export interface IRefreshToken extends Document {
   token?: string;
-  group?: string;
+  Tenant?: any;
   user: any;
   createdAt?: string;
-  updatedAt?: any;
+  updatedAt?: string;
+}
+
+export interface IVerificationToken extends Document {
+  token?: string;
+  user: any;
+  createdAt?: string;
+  updatedAt?: string;
 }
 
 export interface ITenant extends Document {
